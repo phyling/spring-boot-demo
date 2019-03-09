bb87639387ddf1bef533a3bbefd4534016ebe1d9
pengmingde <pengmingde-xf@bestpay.com.cn>
2019-03-09 19:48:52 +0800

    日志框架的相关内容

--- spring-boot-01-hello/src/test/java/com/mengxuegu/PropertiesTest.java
+++ spring-boot-01-hello/src/test/java/com/mengxuegu/PropertiesTest.java
@@ -2,6 +2,7 @@ package com.mengxuegu;
 
 
 import com.mengxuegu.entry.Emp;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,11 +12,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class PropertiesTest {
     @Autowired
     private Emp emp;
     @Test
     public void test(){
         System.out.println(emp);
+    }
+
+    @Test
+    public void testLog(){
+        log.info("info --------");
+        log.info("debug --------");
+        log.error("err --------");
+        log.warn("这是 warn 日志信息");
+        log.trace("trace");
     }
 }
